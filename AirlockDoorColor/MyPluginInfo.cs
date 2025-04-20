5945c17a97f1c90c1c527a93607c1d1c08abbae1
Nagord <46509577+Nagord@users.noreply.github.com>
2025-04-20 16:05:29 -0700

    Updated documentation

--- AirlockDoorColor/MyPluginInfo.cs
+++ AirlockDoorColor/MyPluginInfo.cs
@@ -7,11 +7,11 @@ namespace AirlockDoorColor
         public const string PLUGIN_GUID = $"{PLUGIN_ORIGINAL_AUTHOR}.{PLUGIN_NAME}";
         public const string PLUGIN_NAME = "AirlockDoorColor";
         public const string USERS_PLUGIN_NAME = "Airlock Door Color";
-        public const string PLUGIN_VERSION = "0.0.3";
+        public const string PLUGIN_VERSION = "0.0.4";
         public const string PLUGIN_DESCRIPTION = "Changes the open airlock door button color from green to white. Client Side.";
         public const string PLUGIN_ORIGINAL_AUTHOR = "18107";
         public const string PLUGIN_AUTHORS = "18107, Dragon";
-        public const string PLUGIN_THUNDERSTORE_ID = "VoidCrewModdingTeam/Airlock_Door_Color";
+        public const string PLUGIN_THUNDERSTORE_ID = "NihilityShift/Airlock_Door_Color";
     }
 }
 #pragma warning restore CS1591
